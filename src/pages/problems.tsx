aba24e70283e7ca4501028d28ec1dd5f25db8783
Bright Williams <brightasima@gmail.com>
2022-10-16 13:18:41 +0100

    add problems page for easy viewing

---
+++ src/pages/problems.tsx
@@ -0,0 +1,27 @@
+import { ProblemCard } from ".";
+import { trpc } from "../utils/trpc";
+
+export default function Problems() {
+    const votes = trpc.vote.get.useQuery();
+    const problemsInDb = trpc.problem.get.useQuery();
+    const problems = problemsInDb.data;
+    return <>
+    <div className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4 w-full">
+        <p className="text-2xl text-gray-700 py-5">These are all the problems</p>
+        {
+            problems && problems.map((problem) => {
+                return (
+                    <ProblemCard 
+                        key={problem.id}
+                        description={problem.description} 
+                        votingIsOngoing={false} 
+                        vote={() => {}}
+                        voted={true}
+                        votes={votes.data?.filter((vote) => vote.problemId === problem.id).length || 0}
+                    />
+                )
+            })
+        }
+    </div>
+    </>
+}
